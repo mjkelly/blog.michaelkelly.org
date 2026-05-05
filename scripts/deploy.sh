fbb6b9f02c03e59ec8243fffd3855e5df7cd05e4
Michael Kelly <m@michaelkelly.org>
2026-05-04 22:10:54 -0400

    Sync up prod deploy script with staging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,16 +11,26 @@ scripts=$(dirname $0)
 which aws || (echo "'aws' command not found. Aborting."; exit 2)
 [ -f $cfg ] || (echo "Config file $cfg does not exist. Aborting."; exit 2)
 
-echo "Building..."
+echo "=== Building ==="
 ${scripts}/generate.sh
 
-echo "Deploying..."
-echo "Synchronizing directory $PWD/$dir"
+echo "=== Deploying ==="
+
+echo "Special handling for index.xml (RSS feed)..."
+aws --profile="$profile" \
+  s3 cp "$dir/index.xml" "$bucket/index.xml" \
+  --content-type=application/rss+xml \
+  --cache-control=max-age=3600
+
+echo "Synchronizing directory $PWD/$dir ..."
 aws --profile="$profile" \
   s3 sync "$dir" "$bucket" \
   --cache-control=max-age=3600
-echo "Invalidating CloudFront..."
-aws --profile=admin \
-  cloudfront create-invalidation \
-  --distribution-id "${cf_id}" \
-  --paths "/*"
+
+if [[ -n "$cf_id" ]]; then
+  echo "Invalidating CloudFront..."
+  aws --profile=admin \
+    cloudfront create-invalidation \
+    --distribution-id "${cf_id}" \
+    --paths "/*"
+fi
